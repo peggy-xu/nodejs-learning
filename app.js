541ef9f7fd0cecb88bd8b1eca296c73e13a67b48
peipxu <peipei.xu@nokia-sbell.com>
2018-11-14 15:27:50 +0800

    delete the comment part.
    Signed-off-by: peipxu <peipei.xu@nokia-sbell.com>

--- app.js
+++ app.js
@@ -63,36 +63,5 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// app.dynamicHelpers({
-//   user: function(req, res) {
-//     return req.session.user;
-//   },
-//   error: function(req, res) {
-//     var err = req.flash('error');
-//     if (err.length)
-//       return err;
-//     else
-//       return null;
-//   },
-//   success: function(req, res) {
-//     var succ = req.flash('success');
-//     if (succ.length)
-//       return succ;
-//     else
-//       return null;
-//   },
-// });
-
 module.exports = app;
 app.listen(3000);
